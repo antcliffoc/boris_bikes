a33988c0336414dfb8df46f0d26025ec37496613
Oliver Antcliff <antcliffoc@gmail.com>
2019-08-22 14:40:33 +0100

    Refactor Test Units

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,19 +2,16 @@ require './lib/docking_station'
 
 describe DockingStation do
 
-  describe "creating a DockStation" do
+
     it "creates a new docking station object" do
       expect(DockingStation.new).to be_instance_of DockingStation
     end
-  end
-
-  describe "instance implements" do
 
 
     it "release_bike" do
       expect(DockingStation.new).to respond_to(:release_bike)
     end
-  end
+
 
   it 'gets a bike' do
     bike = Bike.new
